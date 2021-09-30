af5f7d1d9d07c9b0326fa4f617decc4b132cc4f4
NefedovAndry <nefedov_andry@bk.ru>
2021-09-30 21:39:30 +0300

    Ex. 5-1

--- src/main/java/ua/goit/Storage.java
+++ src/main/java/ua/goit/Storage.java
@@ -3,6 +3,7 @@ package ua.goit;
 public class Storage {
     private Publication[] publications;
     private final static int DEFAULT_SIZE = 16;
+    private int cursor = 0;
 
     public Storage() {
         this.publications = new Publication[DEFAULT_SIZE];
@@ -10,5 +11,10 @@ public class Storage {
 
     public Storage(int size) {
         this.publications = new Publication[size];
+    }
+
+    public void add(Publication publication) {
+        this.publications[this.cursor] = publication;
+        this.cursor++;
     }
 }
