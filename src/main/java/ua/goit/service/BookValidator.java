bb3404a77acac3ebc494eaa68a0a29bee5039faa
alexyanov <yanov.alexander@gmail.com>
2022-05-11 20:29:12 +0300

    Add book validation

--- src/main/java/ua/goit/service/BookValidator.java
+++ src/main/java/ua/goit/service/BookValidator.java
@@ -1,14 +1,18 @@
 package ua.goit.service;
 
 import ua.goit.model.ErrorMessage;
+import ua.goit.model.dao.BookDao;
+import ua.goit.repository.BookRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookValidator {
+    private BookRepository bookRepository;
 
-    public BookValidator() {
+    public BookValidator(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     public ErrorMessage validateCreateBook(HttpServletRequest request) {
@@ -24,6 +28,11 @@ public class BookValidator {
         String name = request.getParameter("bookName");
         if (name.isBlank()) {
             errors.add("Book title can not be empty");
+        }
+        List<BookDao> savedBooks = bookRepository.findByName(name);
+
+        if (!savedBooks.isEmpty()) {
+            errors.add(String.format("Book title %s already exist", name));
         }
 
         try {
