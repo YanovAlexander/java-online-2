d892b45084b495f5df0227d833cc76193763bde0
alexyanov <yanov.alexander@gmail.com>
2022-05-18 18:33:59 +0300

    initial spring security

--- src/main/java/ua/goit/controller/IndexController.java
+++ src/main/java/ua/goit/controller/IndexController.java
@@ -2,14 +2,20 @@ package ua.goit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getIndexPage() {
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String getIndexPost() {
         return "index";
     }
 }
