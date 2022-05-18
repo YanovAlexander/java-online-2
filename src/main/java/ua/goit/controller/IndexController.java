d8e720efba9bad9ffc5b86e428d87ba70bd905f1
alexyanov <yanov.alexander@gmail.com>
2022-05-18 21:40:26 +0300

    Add Spring security basic

--- src/main/java/ua/goit/controller/IndexController.java
+++ src/main/java/ua/goit/controller/IndexController.java
@@ -2,6 +2,7 @@ package ua.goit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +11,11 @@ public class IndexController {
 
     @GetMapping
     public String getIndexPage() {
+        return "index";
+    }
+
+    @PostMapping(path = "/")
+    public String getIndex() {
         return "index";
     }
 }
