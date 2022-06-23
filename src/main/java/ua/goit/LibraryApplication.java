d8a0c71799de48834d180b394d684d8bbeed2f7f
YuraCoupe <yurik124@gmail.com>
2022-06-23 13:04:35 +0300

    add  extends SpringBootServletInitializer to LibraryApplication

--- src/main/java/ua/goit/LibraryApplication.java
+++ src/main/java/ua/goit/LibraryApplication.java
@@ -2,11 +2,12 @@ package ua.goit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
-public class LibraryApplication {
+public class LibraryApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
